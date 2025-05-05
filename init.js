2bec74839b9a110c4a7b15c6df4614c9e9e81928
mohamedhassan20000 <dr.jhrawy@gmail.com>
2025-05-05 21:48:13 +0300

    last update with osama

--- init.js
+++
@@ -1,5 +0,0 @@
-const userType = localStorage.getItem("userType");
-console.log(userType);  // أضف هذا السطر للتحقق من القيمة
-if (userType !== "faculty_secretary") { 
-  window.location.href = "index.html"; 
-}
